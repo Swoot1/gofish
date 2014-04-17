e42a75097cf8def03e618ff6b27a7784f07f84d2
Elin Nilsson <elinhejnilsson@gmail.com>
2014-04-17 16:24:56 +0200

    Lagt till Exceptionklass.

---
+++ application/helpers/ExceptionHandlers/ApplicationException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elin
+ * Date: 2014-04-17
+ * Time: 15:58
+ */
+
+namespace GoFish\Application\Helpers\exceptionHandlers;
+
+
+class ApplicationException extends \ErrorException{
+
+} 
