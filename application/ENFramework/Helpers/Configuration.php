8ab41942fe563e8cf5a0bd72e6aaf03f0e461184
Elin Nilsson <elinhejnilsson@gmail.com>
2014-07-07 13:32:27 +0200

    Kommenterat kod.

--- application/ENFramework/Helpers/Configuration.php
+++ application/ENFramework/Helpers/Configuration.php
@@ -6,14 +6,14 @@
  * Time: 21:12
  * To change this template use File | Settings | File Templates.
  */
-require_once 'AutoLoader.php';
 
 // Set where the application is found on disk.
 set_include_path('C:/Users/Elin/repos');
 
-// Set the folder where the saved session files should go. Change the second argument if you want to change it.
+// Set the folder where the saved session files should go. Change the second argument if you want to change the folder.
 ini_set('session.save_path', 'tmp');
 
 // Set up the auto loader.
+require_once 'AutoLoader.php';
 $autoLoader = new \GoFish\Application\ENFramework\Helpers\Autoloader();
 $autoLoader->setUpAutoLoader();
